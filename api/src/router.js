2340801d3b99fc1a6a3ee796da920077dcf2db45
JadaEK <93337558+JadaEK@users.noreply.github.com>
2021-11-01 21:16:34 +0000

    Completed the project

--- api/src/router.js
+++ api/src/router.js
@@ -1,48 +1,49 @@
-const { json } = require('body-parser');
-const { Router } = require('express');
+const {Router} = require('express');
 const multer = require('multer');
 const path = require('path');
-const router = Router();
 const imageProcessor = require('./imageProcessor');
 
-filename = (req, file, callback) => {
-    callback(null, file.originalname);
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
+
+const router = Router();
+
+const filename = (request, file, callback) => {
+  callback(null, file.originalname);
 };
 
-const storage = multer.diskStorage(
-    { destination : 'api/uploads/',
-      filename, }
- );
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename,
+});
 
- const fileFilter = (req, file, callback) => {
-     if (file.mimetype !== 'image/png'){
-         req.fileValidationError = 'Wrong file type';
-         callback(null, false, new Error('Wrong file type'));
-     } else {
-         callback(null, true);
-     }
- };
+const fileFilter = (request, file, callback) => {
+  if (file.mimetype !== 'image/png') {
+    request.fileValidationError = 'Wrong file type';
+    callback(null, false, new Error('Wrong file type'));
+  } else {
+    callback(null, true);
+  }
+};
 
- const upload = multer({
-     fileFilter,
-     storage,
- });
+const upload = multer({
+  fileFilter,
+  storage,
+});
 
- router.post('/upload', upload.single('photo'), async(req, res) =>{
-     if (req.fileValidationError)
-         return res.status(400)
-         .json({error : req.fileValidationError});
-     try{
-         await imageProcessor(req.file.filename);
-     } catch (error){
-   
-     }
-     return res.status(201).json({success : true});
- });
+router.post('/upload', upload.single('photo'), async (request, response) => {
+  if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
 
- const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
- router.get('/photo-viewer', (req,res) => {
-     res.sendFile(photoPath);
- })
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {
+
+  }
+
+  return response.status(201).json({success: true});
+});
+
+router.get('/photo-viewer', (request, response) => {
+  response.sendFile(photoPath);
+});
 
 module.exports = router;
